e21212fdd7a7dcb2c69d600a611d4c745c91b3d5
Gru <ruslru376@gmail.com>
2026-01-10 21:36:28 +1000

    Initial Commit

---
+++ whitemax/whitemaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  whitemaxApp.swift
+//  whitemax
+//
+//  Created by Руслан Артемьев on 10.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct whitemaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
